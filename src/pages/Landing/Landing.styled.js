5224e9be10e508b744c10909428bd259ad88168a
olajyde <olamichael.ade@gmail.com>
2020-08-05 20:20:24 +0100

    Completed product offerings

--- src/pages/Landing/Landing.styled.js
+++ src/pages/Landing/Landing.styled.js
@@ -22,10 +22,93 @@ export const HeroText = styled.h1`
   font-size: ${(props) => props.theme.textSizes.xLarge};
   color: ${(props) => props.theme.colors.tealDark};
   font-family: ${(props) => props.theme.fonts.montserrat};
-  font-weight: ${props => props.theme.fontWeight.bold};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
   padding-top: 6.5rem;
   text-transform: uppercase;
   max-width: 600px;
   margin-bottom: 0;
   line-height: 98px;
+`;
+
+export const ProductSection = styled.section`
+  padding: 80px 0;
+  text-align: center;
+  color: ${(props) => props.theme.colors.tealDark};
+
+  h2 {
+    font-size: 36px;
+    font-family: ${(props) => props.theme.fonts.helvetica};
+    font-weight: ${(props) => props.theme.fontWeight.regular};
+  }
+`;
+
+export const ProductItem = styled.div`
+  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  line-height: 1;
+
+  img {
+    width: 400px;
+    height: 480px;
+    object-fit: contain;
+    z-index: 1;
+  }
+
+  &.reversed {
+    img {
+      margin-left: -9%;
+    }
+  }
+`;
+
+export const ProductDetails = styled.div`
+  margin-left: -9%;
+  position: relative;
+`;
+
+export const NumberText = styled.span`
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+  color: #eeeeee;
+  font-size: 427px;
+`;
+
+export const ProductSpec = styled.article`
+  position: absolute;
+  width: 300px;
+  top: 25%;
+  left: 50%;
+  text-align: left;
+
+  h5 {
+    font-size: 21px;
+    font-family: ${(props) => props.theme.fonts.helvetica};
+    font-weight: ${(props) => props.theme.fontWeight.regular};
+    margin-bottom: 12px;
+    line-height: 1.45;
+  }
+
+  p {
+    line-height: 1.83;
+    font-size: ${(props) => props.theme.textSizes.small};
+  }
+
+  &.reversed {
+    top: 30%;
+    left: 13%;
+    text-align: right;
+  }
+`;
+
+export const Text = styled.h6`
+  font-family: ${(props) => props.theme.fonts.montserrat};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+  color: ${(props) => props.theme.colors.tealLight};
+  letter-spacing: 2.4px;
+  font-size: ${(props) => props.theme.textSizes.small};
+  margin-bottom: 17px;
+  text-transform: uppercase;
 `;
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -3,7 +3,18 @@ import PropTypes from 'prop-types';
 import Button from 'components/Button';
 import Logo from 'components/Logo';
 import Footer from 'components/Footer';
-import { Hero, HeroText } from './Landing.styled';
+import HairLoss from 'assets/hair-photo.png';
+import SexPhoto from 'assets/sex-photo.png';
+import {
+  Hero,
+  HeroText,
+  ProductSection,
+  ProductItem,
+  NumberText,
+  ProductDetails,
+  ProductSpec,
+  Text,
+} from './Landing.styled';
 
 const Landing = ({ startQuiz }) => {
   return (
@@ -22,6 +33,43 @@ const Landing = ({ startQuiz }) => {
         </article>
         <Button onClick={startQuiz}>TAKE THE QUIZ</Button>
       </Hero>
+      <ProductSection>
+        <h2>What we can help with</h2>
+        <ProductItem>
+          <img src={HairLoss} alt='hair loss' />
+          <ProductDetails>
+            <NumberText>01</NumberText>
+            <ProductSpec>
+              <Text>Hair Loss</Text>
+              <h5>Hair loss needn’t be irreversible. We can help!</h5>
+              <p>
+                Hair loss needn’t be irreversible. There’s a scientifically
+                proven way that’s most effective in keeping and regrowing your
+                hair. It’s all to do with blocking a pesky hormone called DHT.
+                That’s the bad guy behind hair loss. And you can keep him at
+                bay. The choice is yours.
+              </p>
+            </ProductSpec>
+          </ProductDetails>
+        </ProductItem>
+        <ProductItem className='reversed'>
+          <ProductDetails>
+            <NumberText>02</NumberText>
+            <ProductSpec className='reversed'>
+              <Text>Erectile Dysfunction</Text>
+              <h5>
+                Erections can be a tricky thing. But no need to feel down!
+              </h5>
+              <p>
+                There are plenty of reasons why you might be having difficulty
+                in the erection department. We can help you figure out possible
+                reasons why. And prescribe a pill if needed.
+              </p>
+            </ProductSpec>
+          </ProductDetails>
+          <img src={SexPhoto} alt='sex' />
+        </ProductItem>
+      </ProductSection>
       <Footer />
     </main>
   );
